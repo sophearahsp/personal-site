8d3e77fe49096793a6fd94e3bfbe43e718c7f138
Sophearah Suy-Puth <sophearahspsp@gmail.com>
2023-10-28 22:24:36 -0400

    base to https

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sophearahsp.github.io/',
+  base: 'https://sophearahsp.github.io/',
   plugins: [react()],
 })
